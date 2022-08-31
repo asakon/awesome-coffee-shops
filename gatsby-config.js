86c4747aa714f4d51439a2b4ff31b9cb64e79b70
Asako Kondo <kondoasako@gmail.com>
2022-08-31 09:50:10 +0900

    GatsbyJSとContenthulを連携する

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,19 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-image',
+    {
+    resolve: 'gatsby-source-contentful',
+    options: {
+      spaceId: process.env.spaceId,
+      accessToken: process.env.accessToken 
+    }
+  }],
 }
